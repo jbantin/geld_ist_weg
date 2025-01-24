8aa6296a6973f5dbc11357df44f77de518c83ea4
k0miker <165369924+k0miker@users.noreply.github.com>
2025-01-24 21:31:07 +0100

    chart prototyp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,13 @@
-// App.tsx
 
-import rawData from "./assets/data/btcs.json"; // Your candlestick data imported here
-
-interface CandleBtcData {
-  openTime: number;
-
-  highPrice: number;
-  openPrice: number;
-  lowPrice: number;
-  closePrice: number;
-}
-
-// openTime: number,
-//   openPrice: 96407.99,
-//   "highPrice": 96411.99,
-//   "lowPrice": 96376,
-//   "closePrice": 96376,
-//   "volume": 8.18452,
-//   "closeTime": 1733011259999,
-//   "quoteAssetVolume": 788996.1628126,
-//   "numberOfTrades": 1763,
-//   "takerBuyBaseVolume": 3.18577,
-//   "takerBuyQuoteVolume": 307128.8909746,
-
-const data = rawData as CandleBtcData[];
+import Chart from "./components/Chart";
 
 const App = () => {
   return (
     <div>
       <h1>huhu</h1>
+      <Chart />
     </div>
   );
 };
 
 export default App;
-// ReactDOM.render(<App />, document.getElementById("root"));
