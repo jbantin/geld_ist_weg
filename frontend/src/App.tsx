4d36a5cfc848671b7a2d2b4e9cc015361aec8f29
Jörg <jbantin@gmx.de>
2025-01-24 20:48:06 +0100

    Co-authored-by: k0miker <k0miker@users.noreply.github.com> Co-authored-by: Denis Röder <Parazim94@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,37 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
+// App.tsx
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <h1 className="text-amber-400 bg-green-500">huhu</h1>
-    </>
-  );
+import rawData from "./assets/data/btcs.json"; // Your candlestick data imported here
+
+interface CandleBtcData {
+  openTime: number;
+
+  highPrice: number;
+  openPrice: number;
+  lowPrice: number;
+  closePrice: number;
 }
 
+// openTime: number,
+//   openPrice: 96407.99,
+//   "highPrice": 96411.99,
+//   "lowPrice": 96376,
+//   "closePrice": 96376,
+//   "volume": 8.18452,
+//   "closeTime": 1733011259999,
+//   "quoteAssetVolume": 788996.1628126,
+//   "numberOfTrades": 1763,
+//   "takerBuyBaseVolume": 3.18577,
+//   "takerBuyQuoteVolume": 307128.8909746,
+
+const data = rawData as CandleBtcData[];
+
+const App = () => {
+  return (
+    <div>
+      <h1>huhu</h1>
+    </div>
+  );
+};
+
 export default App;
+// ReactDOM.render(<App />, document.getElementById("root"));
