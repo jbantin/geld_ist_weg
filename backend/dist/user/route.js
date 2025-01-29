ee4be6ec52cac651d48ec9c045eaf0289d6484d2
Jörg <jbantin@gmx.de>
2025-01-29 23:00:51 +0100

    register/login0.5 added

--- backend/dist/user/route.js
+++ backend/dist/user/route.js
@@ -3,16 +3,40 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.userRoute = void 0;
 const express_1 = require("express");
 const prisma_1 = require("../prisma"); // Pfad zur Prisma-Client-Instanz
+const crypto_1 = require("../lib/crypto");
 exports.userRoute = (0, express_1.Router)();
 // User erstellen
-exports.userRoute.post("/", async (req, res) => {
+exports.userRoute.post("/register", async (req, res) => {
     try {
+        const plainPassword = req.body.password;
+        console.log(plainPassword);
+        req.body.password = await (0, crypto_1.hashPassword)(plainPassword);
         const user = await prisma_1.prisma.user.create({ data: req.body });
         res.json(user);
     }
     catch (error) {
         console.error("Fehlerdetails:", error); // 👈 Fehler ausgeben
         res.status(500).json({ error: "Fehler beim Erstellen des Users" });
+    }
+});
+exports.userRoute.post("/login", async (req, res) => {
+    try {
+        const user = await prisma_1.prisma.user.findUnique({
+            where: { email: req.body.email },
+        });
+        if (!user) {
+            throw new Error("user not found");
+        }
+        if (!(await (0, crypto_1.comparePassword)(req.body.password, user.password))) {
+            throw new Error("wrong password");
+        }
+        //  todo
+        //   jwt zurueck geben
+        res.send(user);
+    }
+    catch (error) {
+        console.error("Fehlerdetails:", error); // 👈 Fehler ausgeben
+        res.status(500).json({ error: "Fehler beim Login" });
     }
 });
 // Alle User abrufen
